be0c247dce951807edff23f18b80ba4a120d7e49
Cata Sandoval <134790635+cosmiccatacorn@users.noreply.github.com>
2024-11-12 08:24:38 -0500

    Create Juego.java
    Clase Juego, creada por Ximena

---
+++ contribucion-ximena/Juego.java
@@ -0,0 +1,12 @@
+public class Juego {
+  int precio;
+  String nombre;
+  int unidades;
+  String clasificacion;
+  Juegos(int precioI, String nombreI, int unidadesI, String clasificacionI){
+    this.precio=precioI;
+    this.nombre=nombreI;
+    this.unidades=unidadesI;
+    this.clasificacion=clasificacionI;
+  }
+}
